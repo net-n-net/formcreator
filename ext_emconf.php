c5f67f66c77341a3b4c6f2341e02b5a2915858c9
Tayfur <tayfur79@yahoo.de>
2019-10-04 10:45:16 +0200

    Add new plugin description

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,9 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Form creator',
-	'description' => 'Creates a form in the frontend',
+	'description' => 'This extension makes it possible to create input forms via an administration page in your website (not in the TYPO3 backend).
+	 Editors can not or should not always edit forms via the TYPO3 backend. With this extension, an administration mask can be created on a front-end page with which forms on web pages can be easily created.
+	 You can create easily your own validations without programming.',
 	'category' => 'plugin',
 	'author' => 'Stefan Westermayer',
 	'author_email' => 'info@netnnet.de',
@@ -12,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '8.0.7',
+	'version' => '8.0.8',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '7.6.0-8.7.99',
